bccbe2dfd36edafb713a190b938f825e959da735
lina <haeydy@163.com>
2017-09-05 15:13:28 +0800

    blog
    add my blog page collectionURL

---
+++ blog/js/canvas.js
@@ -0,0 +1,8 @@
+/*window.onload = canvasApp;
+function canvasApp(){
+  var can = document.getElementById("top-canvas");
+  var ctx = can.getContext("2d");
+  ctx.fillStyle = "#2B2B2B";
+  ctx.fillRect(0, 0, 1583, 200);
+
+}*/
